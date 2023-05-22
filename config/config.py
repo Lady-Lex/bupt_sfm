67eea251240bec967373dd7a05e60beca2366d32
Lexington <3312853823@qq.com>
2023-05-22 21:35:54 +0800

    ✨ feat: _create files

---
+++ config/config.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+img_dir = '../data/Herz-Jesus-P25/images'
+
+K = np.array([[2759.48,       0, 1520.69],
+	      	  [      0, 2764.16, 1006.81],
+			  [      0,       0,       1]])
